a0c641f2f257e539eb74605d3c0fd62772f9914d
sss137 <tlstjdtn137@naver.com>
2025-07-18 16:43:52 +0900

    07.18

---
+++ 01_servlet/src/main/java/chap03_forward/SecondForward.java
@@ -0,0 +1,47 @@
+package chap03_forward;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SecondForward")
+
+public class SecondForward extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+      
+  //FirstForward로부터 전달(forward)된 요청(request)과 응답(response)은 doGet() 메소드가 받습니다.
+  //최초 요청 방식(여기선 a 태그/method="get")이 forward할 때도 그대로 유지되기 때문에 doGet() 메소드가 받습니다.
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	  //요청 파라미터의 전달 확인하기
+	  String p = request.getParameter("p");
+	  
+	  //응답 만들기
+	  response.setHeader("Content-Type", "text/html; charset=UTF-8");
+	  PrintWriter out = response.getWriter();
+	  out.println("<script>");
+	  out.println("alert('" + p + " 전달 완료')");
+	  out.println("history.back()");
+	  out.println("</script>");
+	  out.close();
+	  
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	  doGet(request, response);
+
+	}
+
+}
+
+
+
+
+
---
+++ 01_servlet/src/main/java/chap04_redirect/FirstRedirect.java
@@ -0,0 +1,50 @@
+package chap04_redirect;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FirstRedirect")
+
+public class FirstRedirect extends HttpServlet {
+  
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	  /*
+	   * redirect
+	   * 1. 서버가 클라이언트에게 "새로운 주소로 다시 요청하세요"라고 지시하는 방식입니다.(응답의 일종입니다.)
+	   * 2. redirect 지시를 받은 클라이언트는 다시 새로운 주소로 이동하므로 URL 변경을 확인할 수 있습니다.
+	   * 3. 최초 요청 시 사용한 request와 response는 전달되지 않습니다.(redirect는 별개의 새로운 요청이기 때문입니다.)
+	   * ( = 서버가 redirect를 시킨 후, 클라이언트가 새로 보내는 요청에는 원래의 request/response 정보가 담겨 있지 않다.)
+	   * 4. redirect 시 다른 서버(혹은 애플리케이션)로 이동합니다.
+	   * 5. DB의 데이터 변경 작업(UPDATE, INSERT, DELETE) -> redirect -> 새로 고침 시 중복 제출(서브밋) 등을 방지할 수 있습니다.
+	   *    PRG(Post-Redirect-Get) 패턴을 수행합니다.(redirect 이후 모든 요청은 get방식입니다.)
+	   *
+	   * 단순하게 DB의 데이터 변경 작업(UPDATE, INSERT, DELETE) 했다? redirect
+	   *          SELECT 결과 전달 또는 단순 페이지 이동하고 싶다? forward 
+	   */
+	  
+	  //redierct(새로운 주소로 다시 요청하라는 응답)
+	  response.sendRedirect("/01_servlet/SecondRedirect?p=" + URLEncoder.encode(request.getParameter("p"), "UTF-8")); //context path와 servlet path 모두 작성
+	  
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	  doGet(request, response);
+	
+	}
+
+}
+
+
+
+
+
